3416cf94627e6d50f2055aa45a128a7026cfb202
Ray <loveyou520babytw@gmail.com>
2018-12-13 11:04:39 +0800

    修正

--- js/all.js
+++ js/all.js
@@ -18,39 +18,60 @@ $(document).ready(function () {
         //學員建議寫法
         let todoArray = document.getElementsByClassName('inputdata');
         console.log(todoArray);
-        alert(todoArray[0].value)
         let todo = {
             todoListDate: todoArray[0].value,
             todoListTime: todoArray[1].value,
-            content: todoArray[2].value
+            content: todoArray[2].value,
+            status: false
         };
         ListData.push(todo); //轉換成物件存入
         localStorage.setItem('ListData', JSON.stringify(ListData));
         document.querySelector('.todolist').reset();
-        formList.reset();
         upIndexList(ListData);
     }
-    function removeTodo(e) {
-        e.preventDefault(); //取消默認動作，避免A連結外連跳轉
-        if(e.target.nodeName !== 'I'){return}; //可用tagName或nodeName
-        var index = e.target.dataset.index;
-        ListData.splice(index,1);
-        localStorage.setItem('ListData',JSON.stringify(ListData)); //同步到localStorage
-        upIndexList(ListData); //更新網頁畫面
+    function todoOpiton(e) {
+        console.log(e.target.nodeName);
+        console.log(e.target.dataset.index);
+        if (e.target.nodeName == 'A') {
+            var index = e.target.dataset.index;
+            ListData.splice(index, 1);
+            localStorage.setItem('ListData', JSON.stringify(ListData)); //同步到localStorage
+            upIndexList(ListData); //更新網頁畫面
+        } //可用tagName或nodeName
+
     }
     function upIndexList(item) {
         let itemLen = item.length;
-        let str ='';
+        let str = '';
         for (let i = 0; i < itemLen; i++) {
-            str += `
-            <div class="todo-card active">
+            if (item.status) {
+                str += `
+                <div class="todo-card active">
+                        <div class="card-body">
+                            <input type="checkbox" name="" id="">
+                            <p class="todo-completed">${item[i].content}</p>
+                            <ul class="card-main">
+                                <li class="star"><a href="#"><i class="far fa-star"></i></a></li>
+                                <li class="pen"><a href="#"><i class="fas fa-pen"></i></a></li>
+                                <li><a href="#" data-index='${i}' class="far fa-trash-alt"></a></li>
+                            </ul>
+                        </div>
+                        <div class="card-footer">
+                            <i class="far fa-calendar-alt"></i>${item[i].todoListDate}/${item[i].todoListTime}
+                            <i class="far fa-file"></i>
+                            <i class="far fa-comment"></i>
+                        </div>
+                    </div>`;
+            } else {
+                str += `
+                <div class="todo-card">
                     <div class="card-body">
                         <input type="checkbox" name="" id="">
-                        <p>${item[i].content}</p>
+                        <p class="todo-completed">${item[i].content}</p>
                         <ul class="card-main">
                             <li class="star"><a href="#"><i class="far fa-star"></i></a></li>
                             <li class="pen"><a href="#"><i class="fas fa-pen"></i></a></li>
-                            <li><a href="#" data-index='${i}'><i class="far fa-trash-alt"></i></a></li>
+                            <li><a href="#" data-index='${i}' class="far fa-trash-alt"></a></li>
                         </ul>
                     </div>
                     <div class="card-footer">
@@ -59,10 +80,12 @@ $(document).ready(function () {
                         <i class="far fa-comment"></i>
                     </div>
                 </div>`;
+            }
+
         };
         content.innerHTML = str;
     }
     //event
     btnAdd.addEventListener('click', addTodo);
-    content.addEventListener('click',removeTodo);
+    content.addEventListener('click', todoOpiton);
 });
